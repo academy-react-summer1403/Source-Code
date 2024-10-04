c351385e31af88867fc3271ed49b9e74f71f2780
behrad <166942478+behradbaniadami1@users.noreply.github.com>
2024-10-04 11:43:08 +0330

    install packege coplect

---
+++ src/app/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Landing from "../screens/Landing/Landing";
+import LayOut from "../components/layout/LayOut";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LayOut />,
+      children: [
+        {
+          path: "/",
+          element: <Landing />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
