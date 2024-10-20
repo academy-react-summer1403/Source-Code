aae11ebd7882bb4642c3688c02521246bb6cfe0e
Behrad <behradbaniadami58@gmail.com>
2024-10-20 17:52:09 +0330

    panel layout res fix

--- src/screens/Panel/Panel.jsx
+++ src/screens/Panel/Panel.jsx
@@ -4,7 +4,7 @@ import Avatar from "@mui/material/Avatar";
 import Pagination from "@mui/material/Pagination";
 // import { Login } from "@mui/icons-material";
 import { Dialog, Transition } from "@headlessui/react";
-// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+// import { XMarkIcon } from "@heroicons/react/24/outline";
 import Dashbord from "../../components/panelComponent/Dashbord/Dashbord";
 import EditProfile from "../../components/panelComponent/EditProfile/EditProfile";
 import CheangePass from "../../components/panelComponent/CheagePass/CheangePass";
@@ -67,13 +67,24 @@ function Panel() {
                     </button>
                   </div>
                 </Transition.Child>
-                <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4 bg-rangBg text-asBlack-300">
+                <div className="bg- h-0 px-2 flex-1 overflow-y-auto pt-5 pb-4 text-asBlack-300 bg-indigo-50">
                   <div className="flex flex-shrink-0 items-center px-4"></div>
-                  <div className="z-40 mx-auto xl:h-screen xl:w-80  rounded-lg shadow-xl my-10 relative ">
-                    <div className="mt-5 w-[100%] h-24 flex">
-                      <div className="w-[40%]">
-                        <div className="w-[45%] h-[45%]">
-                          <div className="w-20 h-20 my-auto xl:mt-2 mx-[35%]">
+                  <div className="py-12 z-40 mx-auto xl:h-screen xl:w-80 rounded-lg shadow-xl my-10 relative bg-gradient-to-b from-indigo-500 to-indigo-800">
+                    <svg
+                      className="text-white -mt-4 mr-3"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="23"
+                      height="23"
+                      fill="currentColor"
+                      // class="bi bi-house-door-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
+                    </svg>
+                    <div className="mt-2 pb-2 w-[90%] h-24">
+                      <div className="w-[40%] mx-auto">
+                        <div className="w-min h-min">
+                          <div className="w-min h-min my-auto xl:mt-2 mx-[35%]">
                             <Avatar
                               sx={{ width: 80, height: 80 }}
                               alt="Zahradar"
@@ -82,21 +93,17 @@ function Panel() {
                           </div>
                         </div>
                       </div>
-                      <div className="w-1/2 mr-5">
-                        <div className="text-asBlack-100 font-bold text-xl text-center mt-4">
+                      <div className="w-1/2 mx-auto">
+                        <div className="text-white font-thin text-xl text-center mt-3">
                           {" "}
-                          Zahradar{" "}
-                        </div>
-                        <div className="text-gray-500 text-xs text-center mt-2">
-                          {" "}
-                          زهرا چلویِی دارایی{" "}
+                          بیتا قنبری{" "}
                         </div>
                       </div>
                     </div>
                     <div
                       className={
-                        (count === 1 ? "text-purple-400" : "") +
-                        " mt-10 w-full h-10 flex pr-5 pt-2"
+                        (count === 1 ? "bg-indigo-700" : "") +
+                        " mt-10 mx-auto rounded-md text-white w-[90%] h-10 flex pr-5 pt-2"
                       }
                     >
                       <svg
@@ -120,8 +127,8 @@ function Panel() {
                     </div>
                     <div
                       className={
-                        (count === 2 ? "text-purple-400" : "") +
-                        " mt-3 w-full h-10 flex pr-5 pt-2"
+                        (count === 2 ? "bg-indigo-700" : "") +
+                        " mt-3 mx-auto rounded-md text-white w-[90%] h-10 flex pr-5 pt-2"
                       }
                     >
                       <svg
@@ -149,8 +156,8 @@ function Panel() {
                     </div>
                     <div
                       className={
-                        (count === 5 ? "text-purple-400" : "") +
-                        " mt-3 w-full h-10 flex pr-5 pt-2"
+                        (count === 4 ? "bg-indigo-700" : "") +
+                        " mt-3 mx-auto rounded-md text-white w-[90%] h-10 flex pr-5 pt-2"
                       }
                     >
                       <svg
@@ -164,7 +171,7 @@ function Panel() {
                         <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2M2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
                       </svg>
                       <div
-                        onClick={() => setCount(5)}
+                        onClick={() => setCount(4)}
                         className="mr-2 cursor-pointer"
                       >
                         {" "}
@@ -173,8 +180,8 @@ function Panel() {
                     </div>
                     <div
                       className={
-                        (count === 3 ? "text-purple-400" : "") +
-                        " mt-3 w-full h-10 flex pr-5 pt-2"
+                        (count === 3 ? "bg-indigo-700" : "") +
+                        " mt-3 mx-auto rounded-md text-white w-[90%] h-10 flex pr-5 pt-2"
                       }
                     >
                       <svg
@@ -199,51 +206,20 @@ function Panel() {
                         دوره های من{" "}
                       </div>
                     </div>
-                    <div
-                      className={
-                        (count === 4 ? "text-purple-400" : "") +
-                        " mt-3 w-full h-10 flex pr-5 pt-2"
-                      }
-                    >
+                    <div className="mt-3 mx-auto rounded-md text-white w-[90%] h-10 flex pr-5 pt-2">
                       <svg
                         className="mr-2 mt-2"
                         xmlns="http://www.w3.org/2000/svg"
-                        width="23"
-                        height="23"
-                        fill="currentColor"
-                        class="bi bi-list"
-                        viewBox="0 0 16 16"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-6"
                       >
                         <path
-                          fill-rule="evenodd"
-                          d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-                        />
-                      </svg>
-                      <div
-                        onClick={() => setCount(4)}
-                        className="mr-2 cursor-pointer"
-                      >
-                        {" "}
-                        لیست دوره ها{" "}
-                      </div>
-                    </div>
-                    <div className="mt-3 w-full h-10 flex pr-5 pt-2">
-                      <svg
-                        className="mr-2 mt-2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="23"
-                        height="23"
-                        fill="currentColor"
-                        class="bi bi-box-arrow-right"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
-                        />
-                        <path
-                          fill-rule="evenodd"
-                          d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9"
                         />
                       </svg>
                       <div className="mr-2"> خروج </div>
@@ -266,12 +242,12 @@ function Panel() {
                 <div>
                   {" "}
                   <svg
-                    className="text-white mt-4 mr-3"
+                    className="text-white mt-3 mr-3"
                     xmlns="http://www.w3.org/2000/svg"
                     width="23"
                     height="23"
                     fill="currentColor"
-                    class="bi bi-house-door-fill"
+                    // class="bi bi-house-door-fill"
                     viewBox="0 0 16 16"
                   >
                     <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
@@ -425,12 +401,27 @@ function Panel() {
           </div>
         </div>
         <div className="mx-auto">
-          <div className=" top-0 z-10 bg-rangBg border-lastCoursesBorder rounded-2xl shadow-xl mx-5 my-4 border pl-1 pt-1 sm:pl-3 sm:pt-3 lg:hidden">
+          <div className=" top-0 z-10 border-lastCoursesBorder rounded-2xl shadow-xl mx-5 my-4 border pl-1 pt-1 sm:pl-3 sm:pt-3 lg:hidden">
             <button
               type="button"
               className="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
               onClick={() => setSidebarOpen(true)}
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+
               <span className="sr-only">Open sidebar</span>
               {/* <Bars3Icon className="h-6 w-6" aria-hidden="true" /> */}
             </button>
