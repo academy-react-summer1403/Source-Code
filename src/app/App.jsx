1caf062ec07061f20fe9ec5cbc7b142c32a2472f
Behrad <behradbaniadami58@gmail.com>
2024-12-07 17:32:36 +0330

    develop full

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -8,32 +8,21 @@ import LayOutP from "../components/layout/LayOutP";
 import Panel from "../screens/Panel/Panel";
 import CoursePage from "../components/CourseDetails/CoursePage";
 import { Toaster } from "react-hot-toast";
-<<<<<<< HEAD
-import Blog from "../screens/Blog/Blog";
-// import Register from "../components/Register/Register";
-=======
+// import Blog from "../screens/Blog/Blog";
 import Register from "../components/Register/Register";
->>>>>>> origin/blogss
 import { LoginUser } from "../core/services/api/login-user";
 import { useEffect, useState } from "react";
 import { setItem } from "../core/services/common/storage.services";
 import { getProfile } from "../core/services/api/user";
-<<<<<<< HEAD
-// import ForgetPassword from "../components/form/ForgetPassword";
-// import LoginModal from "../components/form/loginModal";
-// import ForgetPass1 from "../components/form/ForgetPass1";
-// import DaekModeSwich from "../components/DarkModeSwich/DarkmodeSwich";
 import LayOutH from "../components/layout/LayOutH";
 import { SignUp } from "../screens/SignUp";
 import { ForgetPass } from "../screens/ForgetPass";
 // import TheRegister from "../components/Register/TheRegister";
-=======
 import ForgetPassword from "../components/form/ForgetPassword";
 import MainComponent from "../components/Blog/Blogs";
 import BlogDetail from "../components/Blog/BlogDetail";
 
 
->>>>>>> origin/blogss
 
 function App() {
   const router = createBrowserRouter([
--- src/components/panelComponent/Dashbord/Dashbord.jsx
+++ src/components/panelComponent/Dashbord/Dashbord.jsx
@@ -33,13 +33,12 @@ function Dashbord() {
       <div className="h-12 flex border-b border-b-slate-300">
         <div className="w-1/2">
           <div className="w-1/3 h-full">
-            <div className="mt-2 w-max my-auto mr-5 h-min">داشبورد</div>
+            <div className="mt-2 w-max my-auto mr-5 h-min">داشبرد</div>
           </div>
         </div>
         <div className="w-1/2" dir="ltr">
           <div className="w-1/3 h-full flex ml-12">
             <div className="h-min my-auto cursor-pointer">
-<<<<<<< HEAD
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -69,15 +68,6 @@ function Dashbord() {
                   strokeLinejoin="round"
                   d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
                 />
-=======
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-              </svg>
-            </div>
-            <div className="h-min my-auto ml-3 cursor-pointer">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
->>>>>>> origin/blogss
               </svg>
             </div>
           </div>
